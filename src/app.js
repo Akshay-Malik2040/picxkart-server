88c98c6055f4f642d4d5e75d9e2e78bed01de319
Akshay-Malik2040 <akshaymalik2040@gmail.com>
2025-12-24 18:50:58 +0530

    Setting up the environment

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const express=require('express');
+const app=express();
+
+app.use("/",(req,res)=>{
+    res.send("Hello User")
+})
+
+app.listen(3000,()=>{
+    console.log("Server is listening on port 3000")
+})
