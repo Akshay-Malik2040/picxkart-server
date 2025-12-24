1fd44f8ba09d52f9b594f0c8558ac9f4a3fc5dc6
Akshay-Malik2040 <akshaymalik2040@gmail.com>
2025-12-24 19:09:38 +0530

    Connect to Database

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
-const express=require('express');
-const app=express();
+const express = require('express');
+const { connectDB } = require('./config/database')
+const app = express();
+require("dotenv").config()
 
-app.use("/",(req,res)=>{
+app.use("/", (req, res) => {
     res.send("Hello User")
 })
 
-app.listen(3000,()=>{
-    console.log("Server is listening on port 3000")
-})
+connectDB().then(() => {
+    console.log("Database Connected Successfully");
+    app.listen(3000, () => {
+        console.log("Server is listening on port 3000")
+    }) }
+)
